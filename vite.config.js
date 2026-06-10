1d84a3628e8dd194b6b72677159ed130b134b81f
shubha-2018 <thoratshubha2018@gmail.com>
2026-06-10 12:30:33 +0530

    fix vercel build issue

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      "@": "/src"
+    }
+  }
 });
